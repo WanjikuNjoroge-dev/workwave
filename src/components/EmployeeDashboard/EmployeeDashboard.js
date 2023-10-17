929310b829dbca7b848e3e309614453b40d6346d
WanjikuNjoroge-dev <wanjikucareer@gmail.com>
2023-10-17 14:32:44 +0300

    -a

--- src/components/EmployeeDashboard/EmployeeDashboard.js
+++ src/components/EmployeeDashboard/EmployeeDashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import './EmployeeDashboard.css';
 
@@ -6,25 +6,8 @@ const EmployeeDashboard = () => {
   const { isAuthenticated } = useContext(AuthContext);
 
   const [newTask, setNewTask] = useState('');
-  const [newTaskLink, setNewTaskLink] = useState('');
   const [tasks, setTasks] = useState([]);
-
-  // if (!isAuthenticated) {
-  //     return <div>Please register or login to access the Employee Dashboard.</div>;
-  // }
-
-  // const handleAddTask = () => {
-  //   if (newTask.trim()) {
-  //     const taskObj = {
-  //       title: newTask,
-  //       status: 'Not Started',
-  //       hours: 0,
-  //       feedbackRequested: false
-  //     };
-  //     setTasks([...tasks, taskObj]);
-  //     setNewTask('');
-  //   }
-  // }
+  const [submittedTasks, setSubmittedTasks] = useState([]);
 
   const handleAddTask = () => {
     if (newTask.trim()) {
@@ -32,12 +15,10 @@ const EmployeeDashboard = () => {
         title: newTask,
         status: 'Not Started',
         hours: 0,
-        taskLink: newTaskLink,  // Link to the task
         feedbackRequested: false
       };
       setTasks([...tasks, taskObj]);
       setNewTask('');
-      setNewTaskLink(''); // Reset the link field after adding the task
     }
   }
 
@@ -48,16 +29,50 @@ const EmployeeDashboard = () => {
   }
 
   const handleHoursChange = (index, hours) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index].hours = hours;
-    setTasks(updatedTasks);
+    if (tasks[index].status !== 'In Progress' && tasks[index].status !== 'Not Started') {
+      const updatedTasks = [...tasks];
+      updatedTasks[index].hours = hours;
+      setTasks(updatedTasks);
+    }
   }
 
-  const requestFeedback = (index) => {
+  const handleFeedbackRequest = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks[index].feedbackRequested = true;
     setTasks(updatedTasks);
   }
+
+  const handleFormSubmit = () => {
+    // Send tasks data to the database using an API request
+    fetch('http://localhost/workwave/employeedashboardadd.php', {
+      method: 'POST',
+      body: JSON.stringify(tasks),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Tasks submitted:', data);
+        // Reset the tasks once submitted
+        setTasks([]);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  // Use useEffect to fetch and display submitted tasks
+  useEffect(() => {
+    fetch('http://localhost/workwave/employeedashboardretrieve.php')
+      .then((response) => response.json())
+      .then((data) => {
+        setSubmittedTasks(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   return (
     <div className="dashboard-container">
@@ -67,43 +82,83 @@ const EmployeeDashboard = () => {
         <input
           type="text"
           value={newTask}
-          onChange={e => setNewTask(e.target.value)}
+          onChange={(e) => setNewTask(e.target.value)}
           placeholder="Enter new task"
         />
         <button onClick={handleAddTask}>Add Task</button>
       </div>
 
-      <div className="tasks-grid">
-        {tasks.map((task, index) => (
-          <div key={index} className="task-card">
-            <span className="task-title">{task.title}</span>
-            <select value={task.status} onChange={e => handleStatusChange(index, e.target.value)}>
-              <option value="Not Started">Not Started</option>
-              <option value="In Progress">In Progress</option>
-              <option value="Done">Done</option>
-            </select>
-            <input
-              type="number"
-              value={task.hours}
-              onChange={e => handleHoursChange(index, e.target.valueAsNumber)}
-              placeholder="Add hours spent"
-            />
-            {task.taskLink && <a href={task.taskLink} target="_blank" rel="noopener noreferrer">View Work</a>}
-            {!task.feedbackRequested && (
-              <button onClick={() => requestFeedback(index)}>
-                Request Feedback
-              </button>
-            )}
-            {task.feedbackRequested && <span>Feedback Requested</span>}
-          </div>
-        ))}
-      </div>
+      <table className="tasks-table">
+        <thead>
+          <tr>
+            <th>Task Title</th>
+            <th>Status</th>
+            <th>Hours Spent</th>
+            <th>Request Feedback</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task, index) => (
+            <tr key={index}>
+              <td>{task.title}</td>
+              <td>
+                <select
+                  value={task.status}
+                  onChange={(e) => handleStatusChange(index, e.target.value)}
+                >
+                  <option value="Not Started">Not Started</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Done">Done</option>
+                </select>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={task.hours}
+                  onChange={(e) => handleHoursChange(index, e.target.valueAsNumber)}
+                  disabled={task.status === 'In Progress' || task.status === 'Not Started'}
+                />
+              </td>
+              <td>
+                {task.feedbackRequested ? (
+                  <span>Feedback Requested</span>
+                ) : (
+                  <button
+                    onClick={() => handleFeedbackRequest(index)}
+                    disabled={task.status === 'In Progress' || task.status === 'Not Started'}
+                  >
+                    Request Feedback
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-      <div className="feedback-corner">
-        <h3>Supervisor's Feedback</h3>
-        {/* We should get this from backend also ---This is a placeholder. You can render actual feedbacks from the supervisor here. */}
-        <p>Excellent work on the XYZ project. Keep it up!</p>
-      </div>
+      <button onClick={handleFormSubmit}>Submit Tasks</button>
+
+      <h3>Submitted Tasks</h3>
+      <table className="submitted-tasks-table">
+        <thead>
+          <tr>
+            <th>Task Title</th>
+            <th>Status</th>
+            <th>Hours Spent</th>
+            <th>Feedback Requested</th>
+          </tr>
+        </thead>
+        <tbody>
+          {submittedTasks.map((task, index) => (
+            <tr key={index}>
+              <td>{task.title}</td>
+              <td>{task.status}</td>
+              <td>{task.hours}</td>
+              <td>{task.feedbackRequested ? 'Yes' : 'No'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
